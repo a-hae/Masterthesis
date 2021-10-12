ab1c4e09d7a494510e947ae0ed249bead8a6e6f9
a-hae <annette.haenicke@uni-jena.de>
2021-10-12 11:36:42 +0200

    load landuse and lithology polygon

--- 01_load_data.R
+++ 01_load_data.R
@@ -7,7 +7,22 @@ library(RSAGA)
 
 myenv <- rsaga.env()
 
-# 
+# load lithogy and landuse data ################################################
+library(rgdal)
+
+#name of landuse shapefile
+nm_landusepoly <- "landuse_uso_suelo"
+
+#name of lithology shapefile
+nm_lithopoly <- "lithology_cetaquaprj"
+
+# load landuse polygon
+landuse_poly_spain <- readOGR(".", nm_landusepoly)
+
+# load lithology polygon
+litho_poly_spain <- readOGR(".", nm_lithopoly)
+
+
 
 
 
