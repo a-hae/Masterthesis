d3009086459c7dbb396cbefa4ac0f8918ce85efd
a-hae <annette.haenicke@uni-jena.de>
2021-10-12 11:26:11 +0200

    delete function

--- 01_load_data.R
+++ 01_load_data.R
@@ -1,23 +1,13 @@
 setwd("C:/Users/Annette/OneDrive/Master/Masterarbeit/Data/upper_maipo_river")
 
-### load data
-
-# FUNCTIONS ###################################################################
-geoTiffToSGRD_custom <- function(file_name, write_name){
-  #Convert a tif to a SAGA format
-  require(rgdal)
-  setwd("../")
-  spgdf <- readGDAL(file_name)
-  setwd("saga_data")
-  writeGDAL(spgdf, write_name, drivername = "SAGA")
-}
-
 
 # RSAGA SETTINGS ###############################################################
 
 library(RSAGA)
 
 myenv <- rsaga.env()
+
+# 
 
 
 
