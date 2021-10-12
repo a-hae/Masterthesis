5ebe8c689db20f28e99d6fc79a299ee1b79fe170
a-hae <annette.haenicke@uni-jena.de>
2021-10-12 10:12:05 +0200

    new function: convert tif to saga format

--- 01_load_data.R
+++ 01_load_data.R
@@ -1,5 +1,19 @@
-'''
-Masterarbeit:
-Autor: Annette Hänicke
-Oktober 2021
-'''
+setwd("C:/Users/Annette/OneDrive/Master/Masterarbeit/Data/upper_maipo_river")
+
+### load data
+
+# FUNCTIONS ###################################################################
+geoTiffToSGRD_custom <- function(file_name, write_name){
+  #Convert a tif to a SAGA format
+  require(rgdal)
+  setwd("../")
+  spgdf <- readGDAL(file_name)
+  setwd("saga_data")
+  writeGDAL(spgdf, write_name, drivername = "SAGA")
+}
+
+
+
+
+
+
