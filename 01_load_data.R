a2004d6aae219c092a801066485ece2f673f55d2
a-hae <annette.haenicke@uni-jena.de>
2021-10-21 11:35:46 +0200

    add polygon attributes from landuse and lithology shapefile to points

--- 01_load_data.R
+++ 01_load_data.R
@@ -7,7 +7,7 @@ library(rgdal)
 
 # RSAGA SETTINGS ---------------------------------------------------------------
 
-myenv <- rsaga.env()
+env <- rsaga.env()
 
 
 ##load data --------------------------------------------------------------------
@@ -30,7 +30,16 @@ nm_src_pts <- "debris_flow_source_points"
 # load source points shpaefile
 src_pts <- readOGR(".", nm_src_pts)
 
-##
+## landslides in group (landuse and lithology)----------------------------------
+
+# add polygon attributes from landuse and lithology to each point
+rsaga.geoprocessor(lib="shapes_points", module = 10, env = env, show.output.on.console = TRUE, param = list(
+  INPUT = "debris_flow_source_points.shp", OUTPUT = "debris_flow_source_points_poly_attr", POLYGONS = "lithology_cetaquaprj.shp", FIELDS = "Nombre"))
+
+rsaga.geoprocessor(lib="shapes_points", module = 10, env = env, show.output.on.console = TRUE, param = list(
+  INPUT = "debris_flow_source_points_poly_attr.shp", OUTPUT = "debris_flow_source_points_poly_attr", POLYGONS = "landuse_uso_suelo.shp", FIELDS = "Sub_uso"))
+
+
 
 
 
