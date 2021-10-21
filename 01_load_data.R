4a05930e4eb8508017564d52f19da93fcefe7f92
a-hae <annette.haenicke@uni-jena.de>
2021-10-21 15:21:07 +0200

    create new dataframes from landslides for each group (litholgy & landuse)

--- 01_load_data.R
+++ 01_load_data.R
@@ -29,7 +29,7 @@ litho_poly <- readOGR(".", nm_lithopoly)
 nm_src_pts <- "debris_flow_source_points"
 
 
-## landslides in group (landuse and lithology)----------------------------------
+## landslides in groups (landuse and lithology)----------------------------------
 
 # add polygon attributes from landuse and lithology to each point
 rsaga.geoprocessor(lib="shapes_points", module = 10, env = env, show.output.on.console = TRUE, param = list(
@@ -50,9 +50,10 @@ src_pts_attr <- readOGR(".", nm_src_pts_attr)
 src_pts_attr_df <- as.data.frame(src_pts_attr)
 
 # Count landslide in each group
-src_pts_attr_df %>% group_by(Nombre) %>% count()
+litho_groups <- src_pts_attr_df %>% group_by(Nombre) %>% count()
+litho_group_tbl <- data.frame(litho_groups)
 
-src_pts_attr_df %>% group_by(Sub_uso) %>% count()
+landuse_groups <- src_pts_attr_df %>% group_by(Sub_uso) %>% count()
 
 
 
