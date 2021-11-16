6a7b15d445d0ae7f7bfc4094c12c97e753eae4bf
a-hae <annette.haenicke@uni-jena.de>
2021-11-16 12:36:22 +0100

    replace loop with else function

--- merge_slides.R
+++ merge_slides.R
@@ -23,35 +23,39 @@ FindConnected <- function(slidesSHP, func = "touch") {
   # add new attribute: aggregate
   slidesSHP$aggregate = 0
   # define index of aggregate column
-  agg_idx <- which(colnames(slidesSHP@data)=="aggregate")
+  agg_idx <- which(colnames(slidesSHP@data) =="aggregate")
   
-  list = 1:900
-  z = 1
+  list <- 1:900
+  z <- 1
   
   for (s in 1:length(slidesSHP)) {
     for  (k in 1:length(slidesSHP)){
       if (slidesSHP[s,]$Id != slidesSHP[k,]$Id)  {
         # check for same boundary of two features
-        if (func == "touch") {
-          touch <- gTouches(slidesSHP[s,], slidesSHP[k,])
-        }
-        else if (func == "overlaps") {
-          touch <- gOverlaps(slidesSHP[s,], slidesSHP[k,])
-        }
-        
-        if (touch == TRUE) {
+        #if (func == "touch") {
+        #  touch <- gTouches(slidesSHP[s,], slidesSHP[k,])
+        #}
+        #else if (func == "overlaps") {
+        #  touch <- gOverlaps(slidesSHP[s,], slidesSHP[k,])
+        #}
+        touch <- gTouches(slidesSHP[s,], slidesSHP[k,])
+        overlap <- gOverlaps(slidesSHP[s,], slidesSHP[k,])
+        if (touch == TRUE || overlap == TRUE) {
           # give pair of features one value
           # if number already their, take it
           if (slidesSHP[s,]$aggregate == 0 && slidesSHP[k,]$aggregate == 0){
             # else new number
-            slidesSHP[s,agg_idx] = list[z]
-            slidesSHP[k,agg_idx] = list[z]
-            z = z+1
+            slidesSHP[s,agg_idx] <- list[z]
+            slidesSHP[k,agg_idx] <- list[z]
+            z <- z+1
           } else if (slidesSHP[s,]$aggregate == 0 && slidesSHP[k,]$aggregate != 0) {
-            slidesSHP[s,agg_idx] = slidesSHP[k,]$aggregate
+            slidesSHP[s,agg_idx] <- slidesSHP[k,]$aggregate
             
           } else if (slidesSHP[s,]$aggregate != 0 && slidesSHP[k,]$aggregate == 0) {
-            slidesSHP[k,agg_idx] = slidesSHP[s,]$aggregate
+            slidesSHP[k,agg_idx] <- slidesSHP[s,]$aggregate
+            
+          } else if (slidesSHP[s,]$aggregate != 0 && slidesSHP[k,]$aggregate != 0) {
+            slides_con@data[slides_con@data$aggregate == slidesSHP[k,]$aggregate, "aggregate"] <- slidesSHP[s,]$aggregate
             
           }
         }  
@@ -62,18 +66,19 @@ FindConnected <- function(slidesSHP, func = "touch") {
   # change all zero in aggeggrate column in new unique values
   for (m in 1:length(slidesSHP)) {
     if (slidesSHP[m,]$aggregate == 0) {
-      slidesSHP[m,agg_idx] = list[z]
-      z = z+1
+      slidesSHP[m,agg_idx] <- list[z]
+      z <- z+1
     }
   }
   return(slidesSHP)
 }
   
 
-### aggregate connected features -----------------------------------------------
+### merge connected features -----------------------------------------------
 
 #load data
 slides <- readOGR(".","DF_Stolla_all_new")
+
 # identify and define connected slides
 slides_con <- FindConnected(slides)
 
@@ -86,22 +91,24 @@ slides_merged$Id <- 0
 id_idx <- which(colnames(slides_merged@data)=="Id")
 
 for (n in 1:length(slides_merged)) {
-  slides_merged[n,id_idx] = n
+  slides_merged[n,id_idx] <- n
 }
-
-# repeat merging until nothing change
-slides1 <- slides
-slides_merged1 <- slides_merged
-
-while (length(slides1) > length(slides_merged1)) {
-  slides1 = slides_merged
-  slides_merged <- FindConnected(slides1)
-}
-
-# optimize connection of features with gOverlaps function
-slides_merged_fin <- FindConnected(slides_merged, "overlaps")
-
+# 
+# # optimize connection of features with gOverlaps function
+# slides_merged_fin <- FindConnected(slides_merged, "overlaps")
+# 
+# # merge all slides with same number
+# slides_merged_fin <- aggregate(slides_merged_fin, by = "aggregate")
+# 
+# # create new ID column
+# slides_merged_fin$Id <- 0
+# # define index of id column
+# id_idx <- which(colnames(slides_merged_fin@data)=="Id")
+# 
+# for (n in 1:length(slides_merged_fin)) {
+#   slides_merged_fin[n,id_idx] <- n
+# }
 # export merged slides file
-writeOGR(`slides_merged_fin`, dsn = "merged_slides", "merged_slides", driver="ESRI Shapefile", overwrite_layer =  TRUE)
+writeOGR(`slides_merged`, dsn = "merged_slides", "merged_slides", driver="ESRI Shapefile", overwrite_layer =  TRUE)
 
  
