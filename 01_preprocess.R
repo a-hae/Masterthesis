c2d5d20cdadf7942e7311df08398bf460248b621
a-hae <annette.haenicke@uni-jena.de>
2021-11-25 11:10:11 +0100

    create source cells for each polygone

--- 01_preprocess.R
+++ 01_preprocess.R
@@ -35,5 +35,29 @@ dbflows_tree <- dbflows_classtree[dbflows_classtree@data$below_treeline == "Yes"
 
 plot(dbflows_tree)
 
+## create source cells for each runout polygon ---------------------------------
+
+# define proportion of upper cells to select
+upper_prop <- 0.10
+
+src_cells <- autoSelCells(dem = dtm, runout_ply = dbflows_tree[1,], prop= upper_prop, upper = TRUE)
+src_cells[is.na(src_cells)] <- 0
+
+
+for(i in 2:length(dbflows_tree)){
+  #print(i)
+  
+  slide <- dbflows_tree[i,]
+  
+  nxt_src_cells <- autoSelCells(dem = dtm, runout_ply = slide, prop= upper_prop, upper = TRUE)
+  nxt_src_cells[is.na(nxt_src_cells)] <- 0
+  
+  src_cells = src_cells + nxt_src_cells
+  
+}
+
+src_cells[src_cells == 0] <- NA
+
+plot(src_cells)
 
 
