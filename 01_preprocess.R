d9f07c8d12316073589cc32fd312779d14314754
a-hae <annette.haenicke@uni-jena.de>
2021-11-25 11:04:09 +0100

    load data; classify debrisflow above and below treetline with function

--- 01_preprocess.R
+++ 01_preprocess.R
@@ -1,12 +1,39 @@
-library(raster)
-library(sp)
-library(geosphere)
+
+#-------------------------------------------------------------------------------
+# Tasks;
+# - classify above and below treeline
+# - merge all features in shapefile which are connected
+# - classify: define if feature are connected to river or not (column: connected)
+
+###-----------------------------------------------------------------------------
+
+
+## load packages ---------------------------------------------------------------
+
+#library(RSAGA)
+library(rgdal)
 library(rgeos)
+#library(dplyr)
+library(raster)
+
+## load data--------------------------------------------------------------------
 
 setwd("C:/Users/Annette/OneDrive/Master/Masterarbeit/Data/Stolla")
 
-dem <- raster("stolla_dtm5m.tif")
+dtm <- raster("stolla_dtm5m.tif")
+
+dbflows_ply <- readOGR(".","DF_Stolla_all")
 
 source("C:/Users/Annette/OneDrive/Master/Masterarbeit/RProjekt/My_Projekt/Masterthesis/preprocess_fun.R")
+
+
+## classify runout polygons within treeline-------------------------------------
+
+dbflows_classtree <- classTreeline(dtm, dbflows_ply, treeline = 2100)
+
+dbflows_tree <- dbflows_classtree[dbflows_classtree@data$below_treeline == "Yes", "below_treeline"]
+
+plot(dbflows_tree)
+
 
 
