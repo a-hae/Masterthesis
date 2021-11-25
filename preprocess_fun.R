b56459865bdd894a236290a382a59df4ba914ba2
a-hae <annette.haenicke@uni-jena.de>
2021-11-25 11:10:25 +0100

    make it faster

--- preprocess_fun.R
+++ preprocess_fun.R
@@ -16,11 +16,9 @@ classTreeline <- function(dem, runout_ply, treeline) {
   for (i in 1:length(runout_ply)){
     runout_ply_hgts <- extract(dem, runout_ply[i,], method = "simple", df = TRUE)
     # if one value in polygon under value: Yes
-    for (v in runout_ply_hgts[,2]) {
-      if (v <= 1500) {
-        runout_ply[i,tree_idx] <- "Yes"
-        break
-      }
+    check_inside <- runout_ply_hgts <= treeline
+    if (TRUE %in% check_inside[,2]) {
+      runout_ply[i,tree_idx] <- "Yes"
     }
     
   }
