341342ae8f5e43905009e1c55c2789d37190f46f
a-hae <annette.haenicke@uni-jena.de>
2021-12-03 08:33:04 +0100

    load packages and data

---
+++ 03_regional_pcm.R
@@ -0,0 +1,22 @@
+library(runoptGPP)
+library(raster)
+library(rgdal)    
+library(Rsagacmd)
+
+saga <- saga_gis(opt_lib = "sim_geomorphology")
+
+source("C:/Users/Annette/OneDrive/Master/Masterarbeit/RProjekt/func/custom_runoptGPP_fun.R")
+source("C:/Users/Annette/OneDrive/Master/Masterarbeit/RProjekt/func/pcm_conn_fun.R")
+
+# load data---------------------------------------------------------------------
+setwd("C:/Users/Annette/OneDrive/Master/Masterarbeit/Data/Stolla/edit_files")
+
+# runout and source cells polygon ----------------------------------------------
+
+runout_polygons <- readOGR(".", "DF_Stolla_merged_classified")
+src_poly <- readOGR(".", "src_poly_5per")
+
+setwd("../")
+river <- readOGR(".", "Stolla_channel")
+
+dem <- raster("stolla_fillsinks_dtm5m.tif")
