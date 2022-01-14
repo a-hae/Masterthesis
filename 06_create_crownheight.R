1eeb22b145ac7fa97602f054258f8e7654489be0
a-hae <annette.haenicke@uni-jena.de>
2022-01-14 09:56:29 +0100

    create terrain attribute maps

--- 06_create_crownheight.R
+++ 06_create_crownheight.R
@@ -1,5 +1,7 @@
 library(Rsagacmd)
-
+library(rgdal)  
+library(raster)
+library(terra)
 
 saga <- saga_gis( opt_lib = "sim_geomorphology")
 
@@ -17,6 +19,22 @@ hs <- raster("stolla_hs_dtm5m.tif")
 setwd("DSM")
 dsm_raw  <- flip(raster("2010_1m_avg_raster2.tif"), direction = "y")
 
+## create dsm and topography maps ----------------------------------------------
 
+dsm <- terra::resample(dsm_raw, dtm)
+dsm <- mask(dsm, dtm)
 
+# slope and aspect
+slope <- raster::terrain(dsm, opt='slope')
+aspect <- raster::terrain(dsm, opt='aspect')
+
+# create hillshade
+# numbers
+hs <- raster::hillShade(slope, aspect,
+                        angle=40,
+                        direction=270)
+
+plot(hs, col=grey(0:100/100), legend = FALSE, main = "Stolla Basin")
+plot(dsm, add = TRUE, alpha = 0.4)
+plot(runout_polygons, add = TRUE)
 
