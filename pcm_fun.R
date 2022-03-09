dcf3c7a51c2cf4faf25e50bd3c813747ceccbb28
a-hae <annette.haenicke@uni-jena.de>
2022-03-09 14:28:10 +0100

    add functions for pcm

--- pcm_fun.R
+++ pcm_fun.R
@@ -11,8 +11,79 @@
 #- pcmConnGridsearch
 #- pcmGetConnOpt
 #- pcmSpConn
+#- pcmSpConn_nm
+#- pcmspGrid
 #- runoutGeom
 # ------------------------------------------------------------------------------
+runoutGeom <- function(runout_plys, elev, ID = NULL) {
+  #Create a dataframe to store the ID, length, width and (landslide) area
+  if(is.null(ID)){
+    bbDf <- data.frame(fid = 0:(length(runout_plys)-1), id = 1:length(runout_plys),
+                       width = NA, length = NA, area = NA, surfacearea = NA,
+                       maxelev = NA, minelev = NA, reachangle = NA)
+  } else {
+    bbDf <- data.frame(fid = 0:(length(runout_plys)-1), id = NA,
+                       width = NA, length = NA, area = NA, surfacearea = NA,
+                       maxelev = NA, minelev = NA, reachangle = NA)
+  }
+  
+  
+  n_features <- length(runout_plys@polygons)
+  
+  for (i in 1:n_features){
+    runout_ply <- runout_plys[i,]
+    
+    if(!is.null(ID)){bbDf[i,]$id <- runout_ply@data[,ID]}
+    #Get the vertices coordinates of from SpatialPolygons
+    #pnts <- runout_ply@polygons[[1]]@Polygons[[1]]@coords
+    pnts <- getVertices(runout_ply)
+    #Calculate minimum area rectangle
+    bb <- minbb(pnts)
+    
+    #Calcluate the difference in elevation between points
+    elevExt <- raster::extract(elev, bb$box[1:4,])
+    dElev12 <- sqrt( (elevExt[1] - elevExt[2])^2)
+    dElev14 <- sqrt( (elevExt[1] - elevExt[4])^2)
+    
+    if(is.na(dElev14)){
+      #If bounding box node extends beyond dem (e.g. in NA area)
+      dElev14 <- sqrt( (elevExt[2] - elevExt[3])^2)
+    }
+    #Determine (and calculate) the length and width based on delta elevation
+    
+    if(is.na(dElev12)){
+      #If bounding box node extends beyond dem (e.g. in NA area)
+      dElev12 <- sqrt( (elevExt[4] - elevExt[3])^2)
+    }
+    
+    if(dElev12 > dElev14) {
+      length <- EuclDist(bb$box[1,1], bb$box[1,2], bb$box[2,1], bb$box[2,2])
+      width <- EuclDist(bb$box[1,1], bb$box[1,2], bb$box[4,1], bb$box[4,2])
+    } else {
+      length <- EuclDist(bb$box[1,1], bb$box[1,2], bb$box[4,1], bb$box[4,2])
+      width <- EuclDist(bb$box[1,1], bb$box[1,2], bb$box[2,1], bb$box[2,2])
+    }
+    
+    bbDf[i,]$length <- length #planar
+    bbDf[i,]$width <- width #planar
+    bbDf[i,]$area <- rgeos::gArea(runout_ply) #area of landslide (*not area of bbox) {Rgeos}
+    
+    #Calculate the 'true' surface area
+    elevCrop <- raster::crop(elev, runout_ply) #crop and mask used to speed up calculation
+    elevMask <- raster::mask(elevCrop, runout_ply)
+    elevMaskSGDF <- as(elevMask , "SpatialGridDataFrame")
+    bbDf[i,]$surfacearea <- sp::surfaceArea(elevMaskSGDF) #surfacearea of landslide {sp}
+    #Calculate the max. and min. elevation
+    elevPnts <- raster::extract(elev, pnts)
+    bbDf[i,]$maxelev <- max(elevPnts)
+    bbDf[i,]$minelev <- min(elevPnts)
+    bbDf[i,]$reachangle <- 90 - (atan( bbDf[i,]$length / ( bbDf[i,]$maxelev- bbDf[i,]$minelev))*180/pi)
+    
+  }
+  return(bbDf)
+}
+
+
 allTrue <- function(x){ all(x == TRUE)}
 
 errorIndvConn <- function(x, pcm_opt = NULL){
@@ -83,6 +154,15 @@ nintyTrue <- function(x) {
 
 nintyFiveTrue <- function(x) {
   if ((sum(x==TRUE)/length(x)) >= 0.95){
+    return (TRUE)
+  } else{ 
+    return (FALSE)
+  }
+  
+}
+
+seventyTrue <- function(x) {
+  if ((sum(x==TRUE)/length(x)) >= 0.70){
     return (TRUE)
   } else{ 
     return (FALSE)
@@ -185,6 +265,10 @@ pcmConn <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
   length_reldiff <- errMinBox[["rel_difference"]]
   length_error <- errMinBox[["error"]]
   
+  # geometry off debris flow
+  sp.pred <- raster::rasterToPolygons(pred_thres, n = 4, dissolve = TRUE, na.rm=TRUE)
+  geom_pred <- runoutGeom(sp.pred, elev = dem)
+  geom_act <- runoutGeom(slide_poly_single, elev = dem)
   
   # Test if connected
   
@@ -229,6 +313,9 @@ pcmConn <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
       list(id = slide_id,
            conn = is_conn,
            roc = roc,
+           length.pred = geom_pred$length,
+           length.obs = geom_act$length,
+           width.pred = geom_pred$width,
            length.relerr = length_relerr,
            length.reldiff = length_reldiff,
            length.error = length_error,
@@ -244,6 +331,9 @@ pcmConn <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
       list(id =  slide_id,
            conn = is_conn,
            roc = roc,
+           length.pred = geom_pred$length,
+           length.obs = geom_act$length,
+           width.pred = geom_pred$width,
            length.relerr = length_relerr,
            length.reldiff = length_reldiff,
            length.error = length_error))
@@ -320,7 +410,7 @@ pcmConnGridsearch <- function(dem, conn_obj,
 }
 
 
-pcmGetConnOpt <- function(x, performance = "relerr", measure = "median", conn_proportion = "allTrue",
+pcmGetConnOpt <- function(x, performance = "relerr", measure = "median", conn_proportion,
                           from_save = FALSE, plot_opt = FALSE){
   
   # conn_proportion: allTrue, nintyTrue, nintyFiveTrue
@@ -416,7 +506,7 @@ pcmGetConnOpt <- function(x, performance = "relerr", measure = "median", conn_pr
 
 
 pcmGetConnOpt_old <- function(x, performance = "relerr", measure = "median",
-                          from_save = FALSE, plot_opt = FALSE){
+                              from_save = FALSE, plot_opt = FALSE){
   
   if(from_save){
     x <- list()
@@ -572,6 +662,10 @@ pcmSpConn <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
     crop_md_grid <- raster::crop(md_grid, dem_grid)
   }
   
+  crop_mu_grid <- raster::crop(mu_grid, dem_grid)
+  crop_md_grid <- raster::crop(md_grid, dem_grid)
+  
+  
   # Run runout model using Rsagacmd (faster than RSAGA)
   gpp <- saga_lib$sim_geomorphology$gravitational_process_path_model(dem = dem_grid, release_areas = source_grid,
                                                                      friction_mu_grid = crop_mu_grid,
@@ -612,6 +706,11 @@ pcmSpConn <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
   length_relerr <- errMinBox[["rel_error"]]
   length_reldiff <- errMinBox[["rel_difference"]]
   length_error <- errMinBox[["error"]]
+
+  # geometry off debris flow	
+  sp.pred <- raster::rasterToPolygons(pred_thres, n = 4, dissolve = TRUE, na.rm=TRUE)
+  geom_pred <- runoutGeom(sp.pred, elev = dem)
+  geom_act <- runoutGeom(slide_poly_single, elev = dem)
   
   
   # Test if connected
@@ -646,7 +745,7 @@ pcmSpConn <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
     
     sp::plot(rescale_process_area, alpha = 0.4, legend = TRUE, add=TRUE)
     sp::plot(slide_poly_single, add=TRUE)
-    #sp::plot(source_plot, add = TRUE, legend = FALSE)
+   # sp::plot(source_grid, add = TRUE)
     
   }
   
@@ -656,6 +755,9 @@ pcmSpConn <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
       list(id = slide_id,
            conn = is_conn,
            roc = roc,
+           length.pred = geom_pred$length,
+           length.obs = geom_act$length,
+           width.pred = geom_pred$width,
            length.relerr = length_relerr,
            length.reldiff = length_reldiff,
            length.error = length_error,
@@ -671,10 +773,14 @@ pcmSpConn <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
       list(id =  slide_id,
            conn = is_conn,
            roc = roc,
+           length.pred = geom_pred$length,
+           length.obs = geom_act$length,
+           width.pred = geom_pred$width,
            length.relerr = length_relerr,
            length.reldiff = length_reldiff,
            length.error = length_error))
   }
+  
   
 }
 
@@ -745,3 +851,250 @@ runoutGeom <- function(runout_plys, elev, ID = NULL) {
   }
   return(bbDf)
 }
+
+
+pcmSpConn_nm <- function(dem, slide_plys, slide_src, slide_id = 1, conn_obj,
+                         rw_slp = 33, rw_ex = 3, rw_per = 2,
+                         mu_grid, md_grid,
+                         buffer_ext = 500, buffer_source = 50, gpp_iter = 1000,
+                         predict_threshold = 0.5, plot_eval = FALSE,
+                         return_features = FALSE, save_res = FALSE, saga_lib, com_nm)
+{
+  
+  # If single runout polygon as input, assign slide_id value of 1
+  if(length(slide_plys) == 1){
+    slide_id <- 1
+  }
+  
+  slide_plys$objectid <- 1:length(slide_plys)
+  # Subset a single slide polygon
+  slide_poly_single <- slide_plys[slide_id,]
+  
+  # Crop dem to slide polygon
+  dem_grid <- raster::crop(dem, raster::extent(slide_poly_single) + buffer_ext)
+  
+  
+  if(class(slide_src) == "SpatialPointsDataFrame"){
+    # Subset corresponding source/start point of runout
+    sel_over_start_point  <- sp::over(slide_src, slide_poly_single)
+    sel_start_point <- slide_src[!is.na(sel_over_start_point$objectid),]
+    
+    if(!is.null(buffer_source)){
+      # Create a buffer around source point to create a source/release area
+      source_buffer <- rgeos::gBuffer(sel_start_point, width = buffer_source)
+      source_grid <- raster::rasterize(source_buffer, dem_grid, field=1 )
+      source_grid <- raster::mask(source_grid, slide_poly_single )
+      source_plot <- raster::rasterToPolygons(source_grid)
+    } else {
+      # Just use source point
+      source_plot <- sel_start_point
+      source_grid <- raster::rasterize(matrix(sp::coordinates(sel_start_point)[1:2], ncol = 2), dem_grid, field = 1)
+    }
+  }
+  
+  if(class(slide_src) == "SpatialPolygonsDataFrame" ){
+    sel_over_start_poly <- sp::over(slide_src, slide_poly_single)
+    sel_start_poly <- slide_src[!is.na(sel_over_start_poly$objectid),]
+    source_plot <- sel_start_poly
+    source_grid <- raster::rasterize(sel_start_poly, dem_grid, field=1 )
+    
+  }
+  
+  
+  if(class(slide_src) == "RasterLayer"){
+    source_grid <- raster::crop(slide_src, dem_grid)
+    source_grid <- raster::mask(source_grid, slide_poly_single )
+    source_plot <- source_grid
+    crop_mu_grid <- raster::crop(mu_grid, dem_grid)
+    crop_md_grid <- raster::crop(md_grid, dem_grid)
+  }
+  
+  crop_mu_grid <- raster::crop(mu_grid, dem_grid)
+  crop_md_grid <- raster::crop(md_grid, dem_grid)
+  
+  
+  # Run runout model using Rsagacmd (faster than RSAGA)
+  gpp <- saga_lib$sim_geomorphology$gravitational_process_path_model(dem = dem_grid, release_areas = source_grid,
+                                                                     friction_mu_grid = crop_mu_grid,
+                                                                     friction_mass_to_drag_grid = crop_md_grid,
+                                                                     process_path_model = 1,
+                                                                     rw_slope_thres = rw_slp,
+                                                                     rw_exponent = rw_ex,
+                                                                     rw_persistence = rw_per,
+                                                                     gpp_iterations = gpp_iter,
+                                                                     friction_model = 5)
+  #friction_mu = pcm_mu,
+  #friction_mass_to_drag = pcm_md)
+  
+  
+  # Rescale to values from 0 to 1
+  rescale_process_area <- rasterCdf(gpp$process_area)
+  
+  # AUROC
+  pred_values <- raster::getValues(rescale_process_area)
+  pred_values[is.na(pred_values)] <- 0
+  
+  slide_area <- raster::rasterize(slide_poly_single, gpp$process_area, field=1, background = 0)
+  
+  pred_thres <- rescale_process_area
+  pred_thres[pred_thres >= predict_threshold] = 1
+  pred_thres[pred_thres < predict_threshold] = NA
+  
+  pred_area <- ROCR::prediction(predictions = pred_values, labels = raster::getValues(slide_area))
+  perf_area <- ROCR::performance(pred_area, "tpr", "fpr")
+  auroc_area <- ROCR::performance(pred_area, "auc")
+  roc <- auroc_area@y.values[[1]]
+  
+  # Length loss
+  errMinBox <- errMinBboxLength(obs_poly = slide_poly_single,
+                                pred_raster = pred_thres,
+                                dem = dem)
+  
+  length_relerr <- errMinBox[["rel_error"]]
+  length_reldiff <- errMinBox[["rel_difference"]]
+  length_error <- errMinBox[["error"]]
+  
+  # geometry off debris flow
+  sp.pred <- raster::rasterToPolygons(pred_thres, n = 4, dissolve = TRUE, na.rm=TRUE)
+  geom_pred <- runoutGeom(sp.pred, elev = dem)
+  geom_act <- runoutGeom(slide_poly_single, elev = dem)
+  
+  # Test if connected
+  
+  in_obj <- unlist(extract(gpp$process_area, conn_obj))
+  
+  is_conn <- !all(is.na(in_obj))
+  
+  # Plot evaluation results
+  if(plot_eval){
+    
+    slope <- raster::terrain(dem_grid, opt='slope')
+    aspect <- raster::terrain(dem_grid, opt='aspect')
+    # create hillshade
+    # numbers
+    hs <- raster::hillShade(slope, aspect,
+                            angle=40,
+                            direction=270)
+    
+    crop_river <- raster::crop(conn_obj, dem_grid)
+    
+    sp::plot(hs, col=grey.colors(100, start=0, end=1), legend = FALSE,
+             main = paste("id", slide_id, "connected", is_conn,
+                          "roc", round(roc, digits=2), "err.L", round(length_relerr, digits = 2), "\n",
+                          "slp", rw_slp, "exp", rw_ex, "per", rw_per, "mu", "SP", "M/D", "SP"),
+             cex.main = 0.7, cex.axis = 0.7, cex=0.7)
+    
+    if(!is.null(crop_river)){
+      # don't plot river if not in crop area (won't work otherwise)
+      sp::plot(crop_river, col = "#4193CB", add = TRUE)
+    }
+    
+    sp::plot(rescale_process_area, alpha = 0.4, legend = TRUE, add=TRUE)
+    sp::plot(slide_poly_single, add=TRUE)
+    #sp::plot(source_plot, add = TRUE, legend = FALSE)
+    
+  }
+  
+  
+  if(return_features){
+    return(
+      list(id = slide_id,
+           conn = is_conn,
+           roc = roc,
+           length.pred = geom_pred$length,
+           length.obs = geom_act$length,
+           width.pred = geom_pred$width,
+           length.relerr = length_relerr,
+           length.reldiff = length_reldiff,
+           length.error = length_error,
+           dem = dem_grid,
+           actual.poly = slide_poly_single,
+           #source.pnt = sel_start_point,
+           source.area = source_grid,
+           gpp.parea = gpp$process_area,
+           gpp.stop = gpp$stop_positions,
+           gpp.maxvel = gpp$max_velocity))
+  } else {
+    return(
+      list(id =  slide_id,
+           conn = is_conn,
+           roc = roc,
+           length.pred = geom_pred$length,
+           length.obs = geom_act$length,
+           width.pred = geom_pred$width,
+           length.relerr = length_relerr,
+           length.reldiff = length_reldiff,
+           length.error = length_error,
+           combin = com_nm))
+  }
+  
+  
+  result_pcm <- list(id =  slide_id,
+                     conn = is_conn,
+                     roc = roc,
+                     length.pred = geom_pred$length,
+                     length.obs = geom_act$length,
+                     width.pred = geom_pred$width, 
+                     length.relerr = length_relerr,
+                     length.reldiff = length_reldiff,
+                     length.error = length_error,
+                     combin = com_nm)
+  
+  
+  
+  return(result_pcm)
+  
+}
+
+pcmSpGrid <- function(chm_class, pcmmu_vec,dem, slide_plys, slide_src, slide_id = 1, conn_obj,
+                      rw_slp = 33, rw_ex = 3, rw_per = 2,
+                      chm,
+                      buffer_ext = 500, buffer_source = 50, gpp_iter = 1000,
+                      predict_threshold = 0.5, plot_eval = FALSE,
+                      return_features = FALSE, save_res = FALSE, saga_lib) {
+  
+  res <- list()
+  counter <- 1
+  for (m1 in pcmmu_vec) {
+    for (m2 in pcmmu_vec){
+      for (m3 in pcmmu_vec) {
+        for (m4 in pcmmu_vec) {
+          for (m5 in pcmmu_vec) {
+            # difine mu variable for each forest class
+            chm$mu[chm$stolla_crown_hgt_reclass == chm_class[1]] <- m1
+            chm$mu[chm$stolla_crown_hgt_reclass == chm_class[2]] <- m2
+            chm$mu[chm$stolla_crown_hgt_reclass == chm_class[3]] <- m3
+            chm$mu[chm$stolla_crown_hgt_reclass == chm_class[4]] <- m4
+            chm$mu[chm$stolla_crown_hgt_reclass == chm_class[5]] <- m5
+            
+            com <- paste("spmu" ,m1, m2, m3, m4, m5, sep = "_" )
+            
+            
+            res[[counter]] <- pcmSpConn_nm(dem, slide_plys = runout_polygons, slide_src = src_poly, slide_id = poly_id, conn_obj = river,
+                                           rw_slp = rw_opt$rw_slp_opt, rw_ex = rw_opt$rw_exp_opt, rw_per = rw_opt$rw_per_opt,
+                                           mu_grid = chm$mu, md_grid = chm$md,
+                                           buffer_ext = 5000, buffer_source = NULL, gpp_iter = 1000,
+                                           predict_threshold = 0.5, plot_eval = FALSE,
+                                           return_features = FALSE, save_res = TRUE, saga_lib = saga, com_nm = com)
+            
+            counter <- counter + 1
+            #assign(paste("spmu" ,m1, m2,m3, m4, m5, sep = "_" ), ind_perf)
+            # 
+            #file_name <- paste("spmu" ,m1, m2,m3, m4, m5, ".Rd", sep = "_" )
+            # 
+            #save(ind_perf, file = file_name)
+            
+          }
+        }
+      }
+      
+    }
+  }#
+  if(save_res){
+    save(res, file = paste("pcmSPconn_id_", slide_id, ".Rd", sep=""))
+    #save(result_pcm, file = paste("result_pcmconn_gridsearch_", slide_id, ".Rd", sep=""))
+  }
+  
+  return(res)
+}
+
